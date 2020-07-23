694452d1390fbff77d45fa9bbf17d8ed6063e02a
Pavel Varanovich <pavel_varanovich@epam.com>
2020-07-23 17:45:43 +0300

    webpack configuration complete.

---
+++ ui.frontend/src/main/webpack/components/hello-world/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <span>{{message}}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                message: "ХУЕТАsss",
+            }
+        },
+        beforeMount() {
+          console.log("ТУТУТУТУТУТ");
+        },
+    };
+</script>
+
+<style lang="scss" scoped>
+
+   @import "./style/hello";
+
+</style>
