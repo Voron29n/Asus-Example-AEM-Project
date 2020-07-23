31fbf3999a12f6e6c0b8969f131f9b0438fdd991
Pavel Varanovich <pavel_varanovich@epam.com>
2020-07-23 12:38:35 +0300

    add first model from voron

---
+++ core/src/main/java/com/epam/asus/core/models/SimpleModel.java
@@ -0,0 +1,14 @@
+package com.epam.asus.core.models;
+
+public class SimpleModel {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
