0e5e5217189b35609bf6ca72f80a604763051a22
Pavel Varanovich <pavel_varanovich@epam.com>
2020-08-03 11:34:54 +0300

    add class for registration vue components at vue root object

---
+++ ui.frontend/src/main/webpack/site/vue-root/ComponentData.js
@@ -0,0 +1,7 @@
+export class ComponentData {
+    constructor(tagName, selector, importObject) {
+        this.tagName = tagName;
+        this.selector = selector;
+        this.importObject = importObject;
+    }
+}
