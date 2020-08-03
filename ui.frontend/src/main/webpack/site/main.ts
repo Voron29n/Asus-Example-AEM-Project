0eef6ad6287efdf18c78302d435275c4be2a472f
Pavel Varanovich <pavel_varanovich@epam.com>
2020-08-03 11:37:59 +0300

    add import Vue root object in to main ts file

--- ui.frontend/src/main/webpack/site/main.ts
+++ ui.frontend/src/main/webpack/site/main.ts
@@ -4,10 +4,8 @@ import "./main.scss";
 // Javascript or Typescript
 import "./**/*.ts";
 
-// File with import all custom vue components
-import "./vue-components.ts";
-
-import "./main.js";
+// File with registration all custom vue components and create one root Vue obj.
+import "./vue-root/vue-root-object.js";
 
 import * as $ from "jquery";
 import "bootstrap";
