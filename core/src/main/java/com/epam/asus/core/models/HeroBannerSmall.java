2b9b32bbeb6e7a37a785ae2861600285102952de
Pavel Varanovich <pavel_varanovich@epam.com>
2020-08-14 15:02:03 +0300

    add interface and class for hero-banner-small component

---
+++ core/src/main/java/com/epam/asus/core/models/HeroBannerSmall.java
@@ -0,0 +1,14 @@
+package com.epam.asus.core.models;
+
+public interface HeroBannerSmall {
+
+    String getHeroDescription();
+
+    String getHeroTitle();
+
+    String getFileReference();
+
+    String getJsonOfLinkData();
+
+    boolean isEmpty();
+}
