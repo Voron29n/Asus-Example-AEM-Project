9ea473f7361145f7232dfb87e1d6903dd635ed19
Pavel Varanovich <pavel_varanovich@epam.com>
2020-08-15 14:44:50 +0300

    Add HeroBannerSmall vue component and other scss styles

---
+++ ui.frontend/src/main/webpack/v-components/hero-banner-small/HeroBannerSmall.vue
@@ -0,0 +1,51 @@
+<template>
+        <div class="banner-small-block">
+            <div class="banner-small-inner">
+                <v-hero-small
+                    :hero-small-data="heroSmallData"
+                ></v-hero-small>
+            </div>
+        </div>
+</template>
+
+<script>
+import HeroSmall from "./v-components/HeroSmall";
+
+export default {
+    components: {
+        "v-hero-small": HeroSmall,
+    },
+    props: {
+        jsonOfLinkData: {
+            type: String,
+            required: false,
+        },
+        fileReference: {
+            type: String,
+            required: false,
+        },
+        heroTitle: {
+            type: String,
+            required: false,
+        },
+        heroDescription: {
+            type: String,
+            required: false,
+        },
+    },
+    computed: {
+        heroSmallData() {
+            return {
+                heroLink: JSON.parse(this.$props.jsonOfLinkData),
+                fileReference: this.$props.fileReference,
+                heroTitle: this.$props.heroTitle,
+                heroDescription: this.$props.heroDescription,
+            };
+        },
+    },
+};
+</script>
+<style lang="scss">
+@import "./v-style/banner_small_default";
+@import "./v-style/banner_small_media";
+</style>
---
+++ ui.frontend/src/main/webpack/v-components/hero-banner-small/v-components/HeroSmall.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="banner-small-wrap">
+        <a
+            class="af-banner-small-link-block"
+            :href="heroSmallData.heroLink.linkTo"
+            :target="heroSmallData.heroLink.linkUrlTarget"
+        ></a>
+        <div class="banner-small-img">
+            <img :src="heroSmallData.fileReference" />
+        </div>
+        <div class="banner-small-info">
+            <div :class="{'banner-small-info-wrap' : !isDesktopVersion}">
+                <div class="banner-small-title">{{heroSmallData.heroTitle}}</div>
+                <h3 class="banner-small-sub-title">{{heroSmallData.heroDescription}}</h3>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { adapt } from "@mixin/adaptFromDesktopToMobileVersion";
+
+export default {
+    mixins: [adapt],
+    props: {
+        heroSmallData: Object,
+    },
+};
+</script>
+
+<style lang="scss">
+@import "./hero_small_default";
+@import "./hero_small_media";
+</style>
