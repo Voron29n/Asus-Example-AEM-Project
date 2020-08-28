8cfb077ccc907ae8537183ae813cb0795e373c60
Stanislau Slabadzianiuk <Stanislau_Slabadzianiuk@epam.com>
2020-08-28 17:05:56 +0300

    fix color footer new folder content  = ru new template ProArt

--- core/src/main/java/com/epam/asus/core/services/impl/article/ArticleServiceImpl.java
+++ core/src/main/java/com/epam/asus/core/services/impl/article/ArticleServiceImpl.java
@@ -2,7 +2,7 @@ package com.epam.asus.core.services.impl.article;
 
 import com.epam.asus.core.models.beans.article.ImagesBean;
 import com.epam.asus.core.services.ArticleService;
-import com.epam.asus.core.services.CommonUtils;
+import com.epam.asus.core.servlets.CommonUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
 import org.osgi.service.component.annotations.Component;
