675a3f6f64997bd23e743552a9f87a8a6958a327
Stanislau Slabadzianiuk <Stanislau_Slabadzianiuk@epam.com>
2020-08-28 22:12:42 +0300

    add CategoriesNavImpl xml

---
+++ ui.apps/src/main/content/jcr_root/apps/asus/components/custom/proart/categories-nav/v1/categories-nav/clientlibs/dialog/js/categories-nav.js
@@ -0,0 +1,68 @@
+
+(function (window, $, channel, Granite, Coral) {
+    "use strict";
+
+    var selectors = {
+        dialogContent: ".cmp-categories-nav_v1__editor",
+        categories: ".cq-dialog-container__editor-multifield-categories_nav_group",
+    };
+
+    $(document).on("dialog-loaded", function (event) {
+
+        var $dialog = event.dialog;
+
+        if ($dialog.length) {
+            var dialogContent = $dialog[0].querySelector(selectors.dialogContent);
+
+            if (dialogContent) {
+                var categories = dialogContent.querySelector(selectors.categories);
+
+                if (categories) {
+                    Coral.commons.ready(categories, function (e) {
+                        onCategoriesChange(e);
+                    });
+                }
+            }
+        }
+    });
+
+    $(document).on("change", selectors.categories, function (e) {
+        onChangeAddButton(e);
+    });
+
+    function onCategoriesChange(e) {
+        disableButton(e);
+    }
+
+    function onChangeAddButton(e) {
+        let element = e.target;
+        disableButton(element);
+    }
+
+    function disableButton(element) {
+
+        let validationName = element.getAttribute("data-validation");
+        let max = validationName.replace("multifield-max-", "");
+        max = parseInt(max);
+
+        if (element.items.length == max) {
+            let childs = $('#' + element.id).children();
+
+            jQuery.each(childs, function (index, value) {
+                if (value.localName === 'button') {
+                    $(value).attr('disabled', 'disabled');
+                }
+            })
+        } else {
+            let childs = $('#' + element.id).children();
+            jQuery.each(childs, function (index, value) {
+                if (value.localName === 'button') {
+                    $(value).removeAttr('disabled');
+                }
+            })
+        }
+
+    }
+
+})(window, jQuery, jQuery(document), Granite, Coral);
+
