d9555dfdc8a4edb229f8b1f1d3ed4a5e992048c5
Pavel Varanovich <pavel_varanovich@epam.com>
2020-08-30 20:05:37 +0300

    create default vue structure for hero-banner-middle-list component

---
+++ ui.frontend/src/main/webpack/v-components/hero-banner/hero-banner-middle-list/HeroBannerMiddleList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>Hero Banner Middle List</div>
+</template>
+
+<script>
+export default {
+    props: {
+        heroBannerMiddleListItems: {
+            type: String,
+            required: true,
+        }
+    },
+};
+</script>
+<style lang="scss" scoped>
+@import "./v-style/banner_middle_list_default";
+@import "./v-style/banner_middle_list_media";
+</style>
