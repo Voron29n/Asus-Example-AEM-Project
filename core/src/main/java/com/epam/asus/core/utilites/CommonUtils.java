49694960d2a5784f5fdfc9145ed610af04d162d2
Pavel Varanovich <pavel_varanovich@epam.com>
2020-09-02 00:43:56 +0300

    add methods to CommonUtils for work with font awesome icons

--- core/src/main/java/com/epam/asus/core/utilites/CommonUtils.java
+++ core/src/main/java/com/epam/asus/core/utilites/CommonUtils.java
@@ -1,17 +1,27 @@
 package com.epam.asus.core.utilites;
 
+import com.epam.asus.core.models.beans.product.ProductMenuBean;
 import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.epam.asus.core.models.beans.product.ProductMenuBean.DEFAULT_FONT_AWESOME_MENU_ICON_CLASS;
 
 public final class CommonUtils {
     private CommonUtils() {}
 
     private static final String LINK_TO = "linkTo";
     private static final String HTML = ".html";
+
+    private static final String FONT_AWESOME_ICON_PATTERN_WITH_HTML = "^<i class=\"fa[a-z] fa.+\"><\\/i>";
+    private static final String FONT_AWESOME_ICON_PATTERN_WITHOUT_HTML = "^fa[a-z] fa.+";
+    private static final String FONT_AWESOME_PATTERN_CLASS = "fa[a-z] fa.+[^(\"><\\/i>)]";
+
 
     public static boolean isCheckResource(final List<Resource> resources) {
         return resources != null && !resources.isEmpty();
@@ -27,5 +37,35 @@ public final class CommonUtils {
 
     public static String correctLinkByURLFromValueMap(final boolean isExternalLink, final ValueMap vm){
         return isExternalLink ? getPropertyValueByPropertyName(vm, LINK_TO) : getPropertyValueByPropertyName(vm, LINK_TO).concat(HTML);
+    }
+
+    public static String correctFontAwesomeIconClass(String iconClassProp){
+        String iconClassData = null;
+        if (isIconClassWithHtml(iconClassProp)) {
+             iconClassData = cutIconClassFromHtmlWithIconClass(iconClassProp);
+        } else if (isIconClassWithoutHtml(iconClassProp)) {
+            iconClassData = iconClassProp;
+        } else {
+            iconClassData = DEFAULT_FONT_AWESOME_MENU_ICON_CLASS;
+        }
+        return iconClassData;
+    }
+
+    private static boolean isIconClassWithoutHtml(String iconClassProp) {
+        Pattern fontAwesomePattern = Pattern.compile(FONT_AWESOME_ICON_PATTERN_WITHOUT_HTML);
+        Matcher fontAwesomeMatcher = fontAwesomePattern.matcher(iconClassProp);
+        return fontAwesomeMatcher.find();
+    }
+
+    private static String cutIconClassFromHtmlWithIconClass(String iconClassProp) {
+        Pattern fontAwesomePattern = Pattern.compile(FONT_AWESOME_ICON_PATTERN_WITH_HTML);
+        Matcher fontAwesomeMatcher = fontAwesomePattern.matcher(iconClassProp);
+        return iconClassProp.substring(fontAwesomeMatcher.start(),fontAwesomeMatcher.end());
+    }
+
+    private static boolean isIconClassWithHtml(String iconClass) {
+        Pattern fontAwesomePattern = Pattern.compile(FONT_AWESOME_ICON_PATTERN_WITH_HTML);
+        Matcher fontAwesomeMatcher = fontAwesomePattern.matcher(iconClass);
+        return fontAwesomeMatcher.find();
     }
 }
