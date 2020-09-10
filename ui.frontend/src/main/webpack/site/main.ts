f9e22548f5e1495896def4743e45f1a97a453aa3
Stanislau Slabadzianiuk <Stanislau_Slabadzianiuk@epam.com>
2020-09-10 14:24:32 +0300

    corrent main.ts

--- ui.frontend/src/main/webpack/site/main.ts
+++ ui.frontend/src/main/webpack/site/main.ts
@@ -4,11 +4,12 @@ import "./main.scss";
 // Javascript or Typescript
 import "./**/*.ts";
 
-import '@fortawesome/fontawesome-free/js/all.js';
-
 // File with registration all custom vue components and create one root Vue obj.
 import "./vue-root/vue-root-object.js";
 
+// Icons
+import '@fortawesome/fontawesome-free/js/all.js';
 import "./_icomoon.css";
 
+// Images
 import "./resources/images/*.*";
