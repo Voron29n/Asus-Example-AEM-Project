228a083962e20935991035c64579e57a0ff14d8c
Pavel Varanovich <pavel_varanovich@epam.com>
2020-09-11 01:14:02 +0300

    final version for js logic of hero banner middle list component

--- ui.frontend/src/main/webpack/v-components/hero-banner/hero-banner-middle-list/v-components/imagesList/HeroImagesList.vue
+++ ui.frontend/src/main/webpack/v-components/hero-banner/hero-banner-middle-list/v-components/imagesList/HeroImagesList.vue
@@ -12,10 +12,12 @@
                     class="banner-hero-list-item"
                     v-for="(heroMiddleItem, index) in heroBannerMiddleListData"
                     :key="heroMiddleItem.fileReferenceDesktop"
-                    :class="{'active': index == activeImageId }"
+                    :class="{ active: index == activeImageId }"
                 >
                     <VueHeroImg :hero-item="heroMiddleItem"></VueHeroImg>
                 </li>
+
+                
             </div>
         </div>
         <VueSlickDots
@@ -51,20 +53,26 @@ export default {
     data() {
         return {
             activeImageId: 0,
+            defaultStyleValue: {
+                defaultTransition: "all 0.25s ease 0s",
+            },
             componentStyle: {
                 transform: "translateX(0px)",
                 width: `${
-                    window.innerWidth * this.heroBannerMiddleListData.length
+                    document.body.clientWidth *
+                    this.heroBannerMiddleListData.length
                 }px`,
+                transition: "all 0.25s ease 0s",
             },
             isNeedShowNextImg: false,
             timeoutData: {
-                delayTime: 5000, // 5s
+                delayTime: 20000, // 5s
                 timeoutObj: undefined,
             },
             touchMoveData: {
-                positionX: 0,
+                startPositionX: 0,
                 touchMoveX: 0,
+                maxTouchMoveX: document.body.clientWidth * 0.1,
             },
         };
     },
@@ -102,38 +110,55 @@ export default {
             this.componentStyle.transform = `translateX(${
                 this.isDesktopVersion
                     ? 0
-                    : -window.innerWidth * this.activeImageId
+                    : -document.body.clientWidth * this.activeImageId
             }px)`;
             this.componentStyle.width = `${
                 this.isDesktopVersion
-                    ? window.innerWidth
-                    : window.innerWidth * this.heroBannerMiddleListData.length
+                    ? document.body.clientWidth
+                    : document.body.clientWidth *
+                      this.heroBannerMiddleListData.length
             }px`;
         },
-        // configureDraggingState(isDraggingStart, clientX) {
-        //     this.draggingData.isDragging = isDraggingStart;
-        //     this.draggingData.positionX = clientX;
-        //     this.isNeedShowNextImg = false;
-        //     // isDraggingStart
-        //     //     ? clearTimeout(this.timeoutData.timeoutObj)
-        //     //     : this.updateActiveImg();
-        // },
         startTouchMove(event) {
             event.preventDefault();
+            this.componentStyle.transition = "none";
             clearTimeout(this.timeoutData.timeoutObj);
-            this.touchMoveData.positionX = event.touches[0].clientX;
-        },
-        endTouchMove(){
-            this.isNeedShowNextImg = true;
-            this.updateActiveImg();
+
+            this.touchMoveData.startPositionX = event.touches[0].clientX;
         },
         doTouchMove(event) {
-            console.log(event);
             let touchPosititonX = event.touches[0].clientX;
-            let touchMoveX = touchPosititonX - this.touchMoveData.positionX;
+            this.touchMoveData.touchMoveX =
+                touchPosititonX - this.touchMoveData.startPositionX;
             this.componentStyle.transform = `translateX(${
-                -window.innerWidth * this.activeImageId + touchMoveX
+                -document.body.clientWidth * this.activeImageId +
+                this.touchMoveData.touchMoveX
             }px`;
+        },
+        endTouchMove() {
+            if (
+                Math.abs(this.touchMoveData.touchMoveX) > this.touchMoveData.maxTouchMoveX
+            ) {
+                let touchMoveImg = this.touchMoveData.touchMoveX / document.body.clientWidth;
+                let countMoveImgId =
+                    Math.abs(touchMoveImg) < 1
+                        ? 1
+                        : Math.ceil(Math.abs(touchMoveImg));
+                let changeActivateId = this.activeImageId - Math.sign(touchMoveImg) * countMoveImgId;
+                if (changeActivateId < 0) {
+                    this.activeImageId = 0;
+                } else {
+                    let arrayLenght = this.heroBannerMiddleListData.length - 1;
+                    this.activeImageId =
+                        changeActivateId > arrayLenght
+                            ? arrayLenght
+                            : changeActivateId;
+                }
+            }
+            this.updateComponentStyle();
+            this.isNeedShowNextImg = false;
+            this.componentStyle.transition = this.defaultStyleValue.defaultTransition;
+            this.updateActiveImg();
         },
     },
     computed: {
