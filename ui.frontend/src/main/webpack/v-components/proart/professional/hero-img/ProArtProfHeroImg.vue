17e200b1ddd5e8c6ec57f1e78a88d65cce936e67
Pavel Varanovich <pavel_varanovich@epam.com>
2020-09-17 17:16:16 +0300

    final version for ProArtProfHeroImg.vue

--- ui.frontend/src/main/webpack/v-components/proart/professional/hero-img/ProArtProfHeroImg.vue
+++ ui.frontend/src/main/webpack/v-components/proart/professional/hero-img/ProArtProfHeroImg.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        ProArtProfHeroImg with param - {{fileReference}}
+    <div class="prof-hero-img">
+        <div class="prof-hero-img-inner">
+            <img
+                :src="fileReference"
+            />
+        </div>
     </div>
 </template>
 
@@ -14,6 +18,10 @@ export default {
     },
 };
 
-import (/* webpackChunkName: "ProArtProfHeroImg" */ './v-style/_prof_hero_img_default.scss');
-import (/* webpackChunkName: "ProArtProfHeroImg" */ './v-style/_prof_hero_img_media.scss');
+import(
+    /* webpackChunkName: "ProArtProfHeroImg" */ "./v-style/_prof_hero_img_default.scss"
+);
+import(
+    /* webpackChunkName: "ProArtProfHeroImg" */ "./v-style/_prof_hero_img_media.scss"
+);
 </script>
