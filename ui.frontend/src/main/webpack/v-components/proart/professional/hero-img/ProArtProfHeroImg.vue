7cb0c0b352d791f07908aa8395b52f494791cf44
Pavel Varanovich <pavel_varanovich@epam.com>
2020-09-17 13:11:30 +0300

    > create new component: Professional Hero Img > create new Group for this one > create vue part for it

---
+++ ui.frontend/src/main/webpack/v-components/proart/professional/hero-img/ProArtProfHeroImg.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        ProArtProfHeroImg with param - {{fileReference}}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        fileReference: {
+            type: String,
+            required: false,
+        },
+    },
+};
+
+import (/* webpackChunkName: "ProArtProfHeroImg" */ './v-style/_prof_hero_img_default.scss');
+import (/* webpackChunkName: "ProArtProfHeroImg" */ './v-style/_prof_hero_img_media.scss');
+</script>
