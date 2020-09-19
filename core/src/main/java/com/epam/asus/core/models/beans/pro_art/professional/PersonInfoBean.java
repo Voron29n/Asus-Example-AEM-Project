004c996d07f4607aded92d740d32e59677e9851a
Pavel Varanovich <pavel_varanovich@epam.com>
2020-09-19 13:25:24 +0300

    > create PersonInfoBean > create View class for him to create different json

---
+++ core/src/main/java/com/epam/asus/core/models/beans/pro_art/professional/PersonInfoBean.java
@@ -0,0 +1,71 @@
+package com.epam.asus.core.models.beans.pro_art.professional;
+
+import com.epam.asus.core.utilites.View;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import javax.annotation.PostConstruct;
+
+@Getter
+@Model(adaptables = Resource.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class PersonInfoBean {
+
+    @JsonIgnore
+    @SlingObject
+    private Resource currentResource;
+
+    @JsonView(value = View.PersonImg.class)
+    @ValueMapValue
+    private String fileReference;
+    @JsonView(value = View.PersonImg.class)
+    @ValueMapValue
+    private String resourcePath;
+
+    @JsonView(value = View.PersonInfo.class)
+    @ValueMapValue
+    private String firstNameRu;
+    @JsonView(value = View.PersonInfo.class)
+    @ValueMapValue
+    private String lastNameRu;
+    @JsonView(value = View.PersonInfo.class)
+    @ValueMapValue
+    private String firstNameEn;
+    @JsonView(value = View.PersonInfo.class)
+    @ValueMapValue
+    private String lastNameEn;
+    @JsonView(value = View.PersonInfo.class)
+    private String professionPosition;
+    /*field at List - /content/asus/ru/ru/ProArt/Professional*/
+    /*resource field resourcePath - /content/asus/ru/ru/ProArt/Professional/david_Backhem/jcr:content/root/container/person_info*/
+
+    @PostConstruct
+    private void init() {
+        resourcePath = currentResource.getPath();
+    }
+
+    @JsonIgnore
+    public boolean isEmpty() {
+        return StringUtils.isBlank(firstNameRu)
+                || StringUtils.isBlank(firstNameEn)
+                || StringUtils.isBlank(lastNameRu)
+                || StringUtils.isBlank(lastNameEn)
+                || StringUtils.isBlank(professionPosition);
+    }
+
+    @JsonIgnore
+    public boolean isEmptyWithImg() {
+        return StringUtils.isBlank(fileReference)
+                || StringUtils.isBlank(resourcePath)
+                || this.isEmpty();
+    }
+}
---
+++ core/src/main/java/com/epam/asus/core/utilites/View.java
@@ -0,0 +1,6 @@
+package com.epam.asus.core.utilites;
+
+public class View {
+    public static class PersonInfo {}
+    public static class PersonImg extends PersonInfo {}
+}
