19e24b1339954850a22d841bd2647c7387b826d4
Pavel Varanovich <pavel_varanovich@epam.com>
2020-09-22 17:27:52 +0300

    refactor fetchPlugin.js

--- ui.frontend/src/main/webpack/v-components/v-plugin/fetchPlugin.js
+++ ui.frontend/src/main/webpack/v-components/v-plugin/fetchPlugin.js
@@ -2,27 +2,60 @@ export default {
     install(Vue, options) {
         Vue.prototype.$_POST = async function(url, body) {
             let response = await fetch(url, {
-                method: 'POST',
-                cache: 'no-cache',
+                method: "POST",
+                cache: "no-cache",
                 headers: {
-                    'Accept': '*/*',
-                    'Content-Type': 'application/json',
+                    Accept: "*/*",
+                    "Content-Type": "application/json",
                 },
-                body: JSON.stringify(body)
+                body: JSON.stringify(body),
             });
             return response;
         };
 
-        Vue.prototype.$_GET_MODEL = async function(resourcePath) {
-            let response = await fetch(resourcePath + ".model.json", {
-                method: 'GET',
-                cache: 'no-cache',
+        Vue.prototype.$_GET_MODEL = async function(resourcePath, selector, extension) {
+
+            const defaultSelector = `.model`;
+            const defaultExtensions = `.json`;
+
+            let requestSelector = selector !== undefined ? selector : defaultSelector;
+            let requestExtension = extension !== undefined ? extension : defaultExtensions;
+
+            let requestUrl = new URL(resourcePath.concat(requestSelector, requestExtension));
+
+            let response = await fetch(requestUrl, {
+                method: "GET",
+                cache: "no-cache",
                 headers: {
-                    'Accept': '*/*',
-                    'Content-Type': 'application/json',
-                }
+                    Accept: "*/*",
+                    "Content-Type": "application/json",
+                },
             });
             return response;
         };
-    }
+
+        Vue.prototype.$_GET = async function(url, params) {
+            let requestUrl = new URL(url);
+
+            /* EXAMPLE PARAMS */
+            /* var params = { lat: 35.696233, long: 139.570431 }; // or:
+            var params = [
+                ["lat", "35.696233"],
+                ["long", "139.570431"],
+            ]; */
+            if (params !== undefined){
+                requestUrl.search = new URLSearchParams(params).toString();
+            }
+
+            let response = await fetch(requestUrl, {
+                method: "GET",
+                cache: "no-cache",
+                headers: {
+                    Accept: "*/*",
+                    "Content-Type": "application/json",
+                },
+            });
+            return response;
+        };
+    },
 };
